d9ef8689456bca824be3bb6b95acc492b81b8d44
Mauricio Mejia Estevez <mme0504@gmail.com>
2018-08-06 00:09:29 -0500

    v1
    v1

---
+++ Añadir y leer recursos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //LIbreria I and Output
+namespace Añadir_y_leer_recursos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* TextWriter texto = new StreamWriter("prueba.txt");
+            texto.WriteLine("Hola mundo");
+            texto.Close(); */
+
+            //Añadir texto
+            StreamWriter texto = File.AppendText("prueba.txt");
+            texto.WriteLine("Hola mundito");
+            texto.Close();
+
+
+            //Leer texto
+            TextReader texto2 = new StreamReader("prueba.txt");
+            Console.WriteLine(texto2.ReadLine());
+            texto2.Close();
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Añadir y leer recursos/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Añadir y leer recursos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Añadir y leer recursos")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("437d4fcd-4e37-4bf0-9422-30d09c310faa")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
